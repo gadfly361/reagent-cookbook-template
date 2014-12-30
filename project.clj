f017d8886efa0170968f74ee0896245b0bcd3945
matthew <gadfly361@gmail.com>
2014-12-30 12:50:06 -0800

    preparing version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject rc/lein-template "0.1.0-SNAPSHOT"
-  :description "Created with lein-create-template"
-  :url "https://github.com/gadfly361/rc-template"
+(defproject rc/lein-template "0.1.0"
+  :description "Template for Reagent Cookbook recipes"
+  :url "https://github.com/gadfly361/reagent-cookbook-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
  :scm {:name "git"
-       :url "https://github.com/gadfly361/rc-template"}
+       :url "https://github.com/gadfly361/reagent-cookbook-template"}
   :eval-in-leiningen true)
