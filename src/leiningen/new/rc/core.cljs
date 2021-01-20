040e517b7318e75c70b9f3aa3e89ef6c8568dbd8
tangarts <nehemiahcampbell@hotmail.co.uk>
2021-01-20 06:57:50 +0000

    Updated dependencies

--- src/leiningen/new/rc/core.cljs
+++ src/leiningen/new/rc/core.cljs
@@ -1,5 +1,6 @@
 (ns {{ns-name}}.core
-    (:require [reagent.core :as reagent]))
+    (:require [reagent.core :as reagent]
+              [reagent.dom :as rdom]))
 
 (defn home []
   [:div
@@ -7,6 +8,6 @@
    ])
 
 (defn ^:export main []
-  (reagent/render [home]
-                  (.getElementById js/document "app")))
+  (rdom/render [home]
+               (.getElementById js/document "app")))
 
